08e9177fbe190e37e3ab8528c3346d47a2815d70
Kevin Mustelier <kemus@mit.edu>
2014-04-12 20:03:05 -0400

    search results

--- search.js
+++ search.js
@@ -1,14 +1,24 @@
 function searchHandler(e){
-	if(e.which==13||e.keyCode==13){
-		var contents = document.getElementById("searchbox").value;
-		stage='0'
-        	xml_http_post("index.html", stage+contents, search_handle)
-		document.getElementById("searchbox").value = 'downloading song...';
-	}
+    if(e.which==13||e.keyCode==13){
+        var contents = document.getElementById("searchbox").value;
+        stage='0'
+            xml_http_post("index.html", stage+contents, search_handle)
+        document.getElementById("searchbox").value = 'downloading song...';
+    }
 
 }
 
 function search_handle(req) {
-    var contents = document.getElementById("searchitem").value;
+
+    results = eval(req.responseText);
+
+    var table = document.getElementById("searchtable");
+    for(var i=0;i < results.length; i+=1) {
+        row=table.insertRow(-1);
+        left = row.insertCell(0);
+        right= row.insertCell(1);
+        left.innerHTML = results[i]['id'];
+        right.innerHTML = results[i]['title'];
+    }
     debug(req.responseText);
 }
