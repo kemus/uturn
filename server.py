65536d26aee4ad95f6548df3e6ac69c03f313e6c
Kevin Mustelier <kemus@mit.edu>
2014-05-09 03:46:52 -0400

    threaded range simple http server?

--- server.py
+++ server.py
@@ -170,57 +170,120 @@ def youtube_search(strang,max_results):
 
 
 import os
-import posixpath
 import BaseHTTPServer
-import urllib
-import cgi
-import shutil
-import mimetypes
-try:
-    from cStringIO import StringIO
-except ImportError:
-    from StringIO import StringIO
+import SimpleHTTPServer
+
+# Additions for handling Range: header
+import logging
+import re
+
+class InvalidRangeHeader(Exception):
+    pass
+
+def parse_range_header(range_header, total_length):
+    """
+    Return a 2-element tuple containing the requested range offsets
+    in bytes.
+    - range_header is the HTTP header sans the "Range:" prefix
+    - total_length is the length in bytes of the requested resource
+      (needed to calculate offsets for a 'n bytes from the end' request
+    If no Range explicitly requested, returns (None, None)
+    If Range header could not be parsed, raises InvalidRangeHeader
+    (which could either be handled as a user
+    request failure, or the same as if (None, None) was returned
+    """
+    # range_header = self.headers.getheader("Range")
+    if range_header is None or range_header == "":
+        return (None, None)
+    if not range_header.startswith("bytes="):
+        # logging.error("Don't know how to parse Range: %s [1]" %
+        #                (range_header))
+        raise InvalidRangeHeader("Don't know how to parse non-bytes Range: %s" %
+                                 (range_header))
+    regex = re.compile(r"^bytes=(\d*)\-(\d*)$")
+    rangething = regex.search(range_header)
+    if rangething:
+        r1 = rangething.group(1)
+        r2 = rangething.group(2)
+        logging.debug("Requested range is [%s]-[%s]" % (r1, r2))
+
+        if r1 == "" and r2 == "":
+            # logging.warning("Requested range is meaningless")
+            raise InvalidRangeHeader("Requested range is meaningless")
+
+        if r1 == "":
+            # x bytes from the end of the file
+            try:
+                final_bytes = int(r2)
+            except ValueError:
+                raise InvalidRangeHeader("Invalid trailing range")
+            return (total_length-final_bytes, total_length - 1)
+
+        try:
+            from_val = int(r1)
+        except ValueError:
+            raise InvalidRangeHeader("Invalid starting range value")
+        if r2 != "":
+            try:
+                end_val = int(r2)
+            except ValueError:
+                raise InvalidRangeHeader("Invalid ending range value")
+            return (from_val, end_val)
+        else:
+            return (from_val, total_length - 1)
+    else:
+        raise InvalidRangeHeader("Don't know how to parse Range: %s" %
+                                 (range_header))
 
 
-class RangeHTTPRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
-
-    """Simple HTTP request handler with GET and HEAD commands.
-
-    This serves files from the current directory and any of its
-    subdirectories.  The MIME type for files is determined by
-    calling the .guess_type() method.
-
-    The GET and HEAD requests are identical except that the HEAD
-    request omits the actual contents of the file.
+class HTTPRangeRequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
 
     """
+    Extension of SimpleHTTPServer.SimpleHTTPRequestHandler to support
+    the Range header in HTTP requests.  (As needed for serving certain
+    MP3 files to Mobile Safari.
+    """
 
-    server_version = "RangeHTTP"
+    server_version = "HTTPRangeServer/" + __version__
 
     def do_GET(self):
         """Serve a GET request."""
-        f, start_range, end_range = self.send_head()
-        print "Got values of ", start_range, " and ", end_range, "...\n"
+        f = self.send_head()
         if f:
-            f.seek(start_range, 0)
-            chunk = 0x1000
-            total = 0
-            while chunk > 0:
-                if start_range + chunk > end_range:
-                    chunk = end_range - start_range
-                try:
-                    self.wfile.write(f.read(chunk))
-                except:
-                    break
-                total += chunk
-                start_range += chunk
+            if self.range_from is not None and self.range_to is not None:
+                self.copy_chunk(f, self.wfile)
+            else:
+                self.copyfile(f, self.wfile)
             f.close()
 
-    def do_HEAD(self):
-        """Serve a HEAD request."""
-        f, start_range, end_range = self.send_head()
-        if f:
-            f.close()
+    def copy_chunk(self, in_file, out_file):
+        """
+        Copy a chunk of in_file as dictated by self.range_[from|to]
+        to out_file.
+        NB: range values are inclusive so 0-99 => 100 bytes
+        Neither of the file objects are closed when the
+        function returns.  Assumes that in_file is open
+        for reading, out_file is open for writing.
+        If range_tuple specifies something bigger/outside
+        than the size of in_file, out_file will contain as
+        much content as matches.  e.g. with a 1000 byte input,
+        (500, 2000) will create a 500 byte long file
+        (2000, 3000) will create a zero length output file
+        """
+
+        in_file.seek(self.range_from)
+        # Add 1 because the range is inclusive
+        left_to_copy = 1 + self.range_to - self.range_from
+
+        bytes_copied = 0
+        while bytes_copied < left_to_copy:
+            read_buf = in_file.read(left_to_copy)
+            if len(read_buf) == 0:
+                break
+            out_file.write(read_buf)
+            bytes_copied += len(read_buf)
+        return bytes_copied
+
 
     def send_head(self):
         """Common code for GET and HEAD commands.
@@ -241,7 +304,7 @@ class RangeHTTPRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
                 self.send_response(301)
                 self.send_header("Location", self.path + "/")
                 self.end_headers()
-                return (None, 0, 0)
+                return None
             for index in "index.html", "index.htm":
                 index = os.path.join(path, index)
                 if os.path.exists(index):
@@ -257,151 +320,40 @@ class RangeHTTPRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
             f = open(path, 'rb')
         except IOError:
             self.send_error(404, "File not found")
-            return (None, 0, 0)
-        if "Range" in self.headers:
+            return None
+
+        fs = os.fstat(f.fileno())
+        total_length = fs[6]
+        try:
+            self.range_from, self.range_to = parse_range_header(
+                self.headers.getheader("Range"), total_length)
+        except InvalidRangeHeader, e:
+            # Just serve them the whole file, although it's possibly
+            # more correct to return a 4xx error?
+            logging.warning("Range header parsing failed, "
+                            "serving complete file")
+            self.range_from = self.range_to = None
+
+        if self.range_from is not None or self.range_to is not None:
             self.send_response(206)
+            self.send_header("Accept-Ranges", "bytes")
         else:
             self.send_response(200)
-        self.send_header("Content-type", ctype)
-        fs = os.fstat(f.fileno())
-        size = int(fs[6])
-        start_range = 0
-        end_range = size
-        self.send_header("Accept-Ranges", "bytes")
-        if "Range" in self.headers:
-            s, e = self.headers['range'][6:].split('-', 1)
-            sl = len(s)
-            el = len(e)
-            if sl > 0:
-                start_range = int(s)
-                if el > 0:
-                    end_range = int(e) + 1
-            elif el > 0:
-                ei = int(e)
-                if ei < size:
-                    start_range = size - ei
-        self.send_header("Content-Range", 'bytes ' + str(start_range) + '-' + str(end_range - 1) + '/' + str(size))
-        self.send_header("Content-Length", end_range - start_range)
+        self.send_header("Content-Type", ctype)
+        if self.range_from is not None or self.range_to is not None:
+            # TODO: Should also check that range is within the file size
+            self.send_header("Content-Range",
+                             "bytes %d-%d/%d" % (self.range_from,
+                                                 self.range_to,
+                                                 total_length))
+            # Add 1 because ranges are inclusive
+            self.send_header("Content-Length",
+                             (1 + self.range_to - self.range_from))
+        else:
+            self.send_header("Content-Length", str(total_length))
         self.send_header("Last-Modified", self.date_time_string(fs.st_mtime))
         self.end_headers()
-        print "Sending Bytes ",start_range, " to ", end_range, "...\n"
-        return (f, start_range, end_range)
-
-    def list_directory(self, path):
-        """Helper to produce a directory listing (absent index.html).
-
-        Return value is either a file object, or None (indicating an
-        error).  In either case, the headers are sent, making the
-        interface the same as for send_head().
-
-        """
-        try:
-            list = os.listdir(path)
-        except os.error:
-            self.send_error(404, "No permission to list directory")
-            return None
-        list.sort(key=lambda a: a.lower())
-        f = StringIO()
-        displaypath = cgi.escape(urllib.unquote(self.path))
-        f.write('<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 3.2 Final//EN">')
-        f.write("<html>\n<title>Directory listing for %s</title>\n" % displaypath)
-        f.write("<body>\n<h2>Directory listing for %s</h2>\n" % displaypath)
-        f.write("<hr>\n<ul>\n")
-        for name in list:
-            fullname = os.path.join(path, name)
-            displayname = linkname = name
-            # Append / for directories or @ for symbolic links
-            if os.path.isdir(fullname):
-                displayname = name + "/"
-                linkname = name + "/"
-            if os.path.islink(fullname):
-                displayname = name + "@"
-                # Note: a link to a directory displays with @ and links with /
-            f.write('<li><a href="%s">%s</a>\n'
-                    % (urllib.quote(linkname), cgi.escape(displayname)))
-        f.write("</ul>\n<hr>\n</body>\n</html>\n")
-        length = f.tell()
-        f.seek(0)
-        self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        self.send_header("Content-Length", str(length))
-        self.end_headers()
-        return (f, 0, length)
-
-    def translate_path(self, path):
-        """Translate a /-separated PATH to the local filename syntax.
-
-        Components that mean special things to the local file system
-        (e.g. drive or directory names) are ignored.  (XXX They should
-        probably be diagnosed.)
-
-        """
-        # abandon query parameters
-        path = path.split('?',1)[0]
-        path = path.split('#',1)[0]
-        path = posixpath.normpath(urllib.unquote(path))
-        words = path.split('/')
-        words = filter(None, words)
-        path = os.getcwd()
-        for word in words:
-            drive, word = os.path.splitdrive(word)
-            head, word = os.path.split(word)
-            if word in (os.curdir, os.pardir): continue
-            path = os.path.join(path, word)
-        return path
-
-    def copyfile(self, source, outputfile):
-        """Copy all data between two file objects.
-
-        The SOURCE argument is a file object open for reading
-        (or anything with a read() method) and the DESTINATION
-        argument is a file object open for writing (or
-        anything with a write() method).
-
-        The only reason for overriding this would be to change
-        the block size or perhaps to replace newlines by CRLF
-        -- note however that this the default server uses this
-        to copy binary data as well.
-
-        """
-        shutil.copyfileobj(source, outputfile)
-
-    def guess_type(self, path):
-        """Guess the type of a file.
-
-        Argument is a PATH (a filename).
-
-        Return value is a string of the form type/subtype,
-        usable for a MIME Content-type header.
-
-        The default implementation looks the file's extension
-        up in the table self.extensions_map, using application/octet-stream
-        as a default; however it would be permissible (if
-        slow) to look inside the data to make a better guess.
-
-        """
-
-        base, ext = posixpath.splitext(path)
-        if ext in self.extensions_map:
-            return self.extensions_map[ext]
-        ext = ext.lower()
-        if ext in self.extensions_map:
-            return self.extensions_map[ext]
-        else:
-            return self.extensions_map['']
-
-    if not mimetypes.inited:
-        mimetypes.init() # try to read system mime.types
-    extensions_map = mimetypes.types_map.copy()
-    extensions_map.update({
-        '': 'application/octet-stream', # Default
-        '.py': 'text/plain',
-        '.c': 'text/plain',
-        '.h': 'text/plain',
-        '.mp4': 'video/mp4',
-        '.ogg': 'video/ogg',
-        })
-
+        return f
 
 
 class TestHandler(RangeHTTPRequestHandler):
